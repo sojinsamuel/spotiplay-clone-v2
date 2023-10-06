a93ce9b40935edf3bacb63bef6138c2fe5204fa4
nuhptr <nugrahaadi733@gmail.com>
2023-10-06 18:33:24 +0700

    comp: header add useAuthModal

--- components/header.tsx
+++ components/header.tsx
@@ -5,6 +5,9 @@ import { twMerge } from 'tailwind-merge'
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx'
 import { HiHome } from 'react-icons/hi'
 import { BiSearch } from 'react-icons/bi'
+
+import useAuthModal from '@/hooks/use-auth-modal'
+
 import Button from './button'
 
 interface IHeaderProps {
@@ -14,6 +17,7 @@ interface IHeaderProps {
 
 const Header: React.FC<IHeaderProps> = ({ children, className }) => {
    const router = useRouter()
+   const authModal = useAuthModal()
 
    const handleLogOut = () => {
       // handle logout in the future
@@ -48,13 +52,13 @@ const Header: React.FC<IHeaderProps> = ({ children, className }) => {
                <>
                   <div>
                      <Button
-                        onClick={() => {}}
+                        onClick={authModal.onOpen}
                         className='bg-transparent text-neutral-300 font-medium'>
                         Sign up
                      </Button>
                   </div>
                   <div>
-                     <Button onClick={() => {}} className='bg-white px-6 py-2'>
+                     <Button onClick={authModal.onOpen} className='bg-white px-6 py-2'>
                         Log in
                      </Button>
                   </div>
