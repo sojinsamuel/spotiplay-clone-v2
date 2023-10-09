80829229a74da567391036a0e3be6bfb238fb5f1
nuhptr <nugrahaadi733@gmail.com>
2023-10-09 20:15:31 +0700

    actions: add get songs

---
+++ actions/get-song-by-user-id.ts
@@ -0,0 +1,25 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+import { Song } from '@/types/general-types'
+
+const getSongByUserId = async (): Promise<Song[]> => {
+   const supabase = createServerComponentClient({ cookies: cookies })
+
+   const { data: SessionData, error: sessionError } = await supabase.auth.getSession()
+   if (sessionError) {
+      console.error(sessionError)
+      return []
+   }
+
+   const { data, error } = await supabase
+      .from('songs')
+      .select('*')
+      .eq('user_id', SessionData?.session?.user.id)
+      .order('created_at', { ascending: false })
+   if (error) console.error(error)
+
+   return (data as any) || []
+}
+
+export default getSongByUserId
