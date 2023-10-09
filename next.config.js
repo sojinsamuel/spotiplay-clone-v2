349c1106befc42d3bbf109f5881697316a4ccc37
nuhptr <nugrahaadi733@gmail.com>
2023-10-09 20:14:57 +0700

    add images domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+   images: {
+      domains: ['xwgivctaddsyoltyqyqq.supabase.co'],
+   },
+}
 
 module.exports = nextConfig
