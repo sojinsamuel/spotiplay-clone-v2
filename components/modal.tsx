e0bc934a1124418a294b3fb68477d1e0eafcbd9a
nuhptr <nugrahaadi733@gmail.com>
2023-10-09 10:07:50 +0700

    comp: modal headwind formatting

--- components/modal.tsx
+++ components/modal.tsx
@@ -9,16 +9,22 @@ interface IModalProps {
    children?: React.ReactNode
 }
 
-const Modal: React.FC<IModalProps> = ({ isOpen, onChange, title, description, children }) => {
+const Modal: React.FC<IModalProps> = ({
+   isOpen,
+   onChange,
+   title,
+   description,
+   children,
+}) => {
    return (
       <Dialog.Root open={isOpen} defaultOpen={isOpen} onOpenChange={onChange}>
          <Dialog.Portal>
-            <Dialog.Overlay className='bg-neutral-900/90 backdrop-blur-sm fixed inset-0' />
+            <Dialog.Overlay className='fixed inset-0 bg-neutral-900/90 backdrop-blur-sm' />
             <Dialog.Content className='fixed drop-shadow-md border border-neutral-700 top-[50%] left-[50%] max-h-full h-full md:h-auto md:max-h-[85vh] w-full md:w-[90vw] md:max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-md bg-neutral-800 p-[25px] focus:outline-none'>
-               <Dialog.Title className='text-xl text-center font-bold mb-4 text-white'>
+               <Dialog.Title className='mb-4 text-xl font-bold text-center text-white'>
                   {title}
                </Dialog.Title>
-               <Dialog.Description className='text-white mb-5 text-sm leading-normal text-center'>
+               <Dialog.Description className='mb-5 text-sm leading-normal text-center text-white'>
                   {description}
                </Dialog.Description>
                <div>{children}</div>
