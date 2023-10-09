fddd619faf52236207c7569c8fc421870fa24677
nuhptr <nugrahaadi733@gmail.com>
2023-10-09 10:06:07 +0700

    page: headwind formatting

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,18 +3,18 @@ import ListItem from '@/components/list-item'
 
 export default function Home() {
    return (
-      <section className='bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto'>
+      <section className='w-full h-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900'>
          <Header>
             <div className='mb-2'>
-               <h1 className='text-white text-3xl font-semibold'>Welcome Back</h1>
-               <div className='grid grid-cols-1 sm:grid-cols-1 xl:grid-cols-3 gap-3 mt-4'>
+               <h1 className='text-3xl font-semibold text-white'>Welcome Back</h1>
+               <div className='mt-4 grid grid-cols-1 sm:grid-cols-1 xl:grid-cols-3 gap-3'>
                   <ListItem image='/images/liked.png' name='Liked Song' href='liked' />
                </div>
             </div>
          </Header>
-         <div className='mt-2 mb-7 px-6'>
-            <div className='flex justify-between items-center'>
-               <h1 className='text-white text-2xl font-semibold'>Newest Song</h1>
+         <div className='px-6 mt-2 mb-7'>
+            <div className='flex items-center justify-between'>
+               <h1 className='text-2xl font-semibold text-white'>Newest Song</h1>
             </div>
             <div className='text-white'>List Of Songs!</div>
          </div>
