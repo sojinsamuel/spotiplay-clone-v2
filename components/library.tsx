09c1329a08dcffb4761f96636ed93eccca7eeb46
nuhptr <nugrahaadi733@gmail.com>
2023-10-09 10:10:14 +0700

    comp: library update code

--- components/library.tsx
+++ components/library.tsx
@@ -3,9 +3,22 @@
 import { TbPlaylist } from 'react-icons/tb'
 import { AiOutlinePlus } from 'react-icons/ai'
 
+import { useUser } from '@/hooks/use-user'
+import useAuthModal from '@/hooks/use-auth-modal'
+import useUploadModal from '@/hooks/use-upload-modal'
+
 const Library = () => {
+   const authModal = useAuthModal()
+   const uploadModal = useUploadModal()
+   const { user, subscription } = useUser()
+
    const onClick = () => {
-      // handle upload later
+      if (!user) return authModal.onOpen()
+
+      // TODO: check if user has subscription
+
+      // upload modal
+      return uploadModal.onOpen()
    }
 
    return (
@@ -13,15 +26,15 @@ const Library = () => {
          <div className='flex items-center justify-between px-5 pt-4'>
             <div className='inline-flex items-center gap-x-2'>
                <TbPlaylist className='text-neutral-400' size={26} />
-               <p className='text-neutral-400 font-medium text-md'>Your Library</p>
+               <p className='font-medium text-neutral-400 text-md'>Your Library</p>
             </div>
             <AiOutlinePlus
                onClick={onClick}
                size={20}
-               className='text-neutral-400 cursor-pointer hover:text-white transition'
+               className='transition cursor-pointer text-neutral-400 hover:text-white'
             />
          </div>
-         <div className='flex flex-col gap-y-2 mt-4 px-5'>List of songs</div>
+         <div className='flex flex-col px-5 mt-4 gap-y-2'>List of songs</div>
       </div>
    )
 }
